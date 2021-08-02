a3432d6190f00ed1dd4e8bef571322cf7407e389
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2021-08-01 23:58:46 -0300

    Finish basic calculator

--- main.c
+++ main.c
@@ -4,13 +4,14 @@
 enum Estado { INICIAL, NUM1, NUM2, REG1, REG1A, REG2, REG2A, NUM2REG2, ESPERA, TERMINAL };
 
 // Define um valor numerico para cada um dos seis tipos de eventos possiveis
-enum TipoDeEvento { DIGITO, OP, R, TIL, X, ENTER, P };
+enum TipoDeEvento { DIGITO, OP_ARIT, IGUAL, R, TIL, X, ENTER, P };
 
 #define NUM_REG 10
 #define NUM_LINHAS 0x1024
 #define COMPRIMENTO_MAX_LINHA 0x32
 
 unsigned char mem[NUM_LINHAS*COMPRIMENTO_MAX_LINHA];  // memoria programável
+unsigned int regs[NUM_REG]; // registradores
 int estado = INICIAL;  // estado da maquina de estados
 int charEvento = 0;    // char lido da entrada padrao (stdin)
 int tipoDeEvento = 0;  // tipo de evento associado ao char (ARROBA, L, X, ...)
@@ -20,6 +21,7 @@ int acumulador = 0;  //
 int acumuladorInterno = 0;  // usado na leitura de numeros do usuario
 int operando1 = 0;   // primeiro operando
 int operando2 = 0;   // segundo operando
+int ulitmoReg = 0;   // guarda o ultimo registrador referenciado em algum operando
 
 /********************************
  *********** REACOES ************
@@ -34,12 +36,20 @@ void reacaoErro() {
 void reacaoAtualizaAcumulador() {
     acumulador = acumuladorInterno;
     acumuladorInterno = 0;
+    printf("> %d\n\n", acumulador);
 }
 
 // Armazena o digito fornecido
 void reacaoLerDigito() {
     acumuladorInterno = acumuladorInterno*10 + (charEvento - '0');
 }
+
+// Armazena o valor do registrador especificado
+void reacaoLerRegistrador() {
+    ulitmoReg = charEvento - '0';
+    acumuladorInterno = regs[ulitmoReg];
+}
+
 
 // Armazena a operação fornecida
 void reacaoLerOperacao() {
@@ -76,8 +86,11 @@ void reacaoRealizaOperacao() {
     else if(operacao == '/'){
         acumulador = operando1 / operando2;
     }
-    printf("%d %d\n", operando1, operando2);
-    printf("%d\n\n", acumulador);
+    else if(operacao == '='){
+        regs[ulitmoReg] = operando1;
+    }
+    // printf("%d %d\n", operando1, operando2);
+    printf("> %d\n\n", acumulador);
 }
 
 typedef struct {
@@ -86,96 +99,105 @@ typedef struct {
 } Reacao;
 
 // REACOES[s][e] = reacao a partir do estado "s" e evento de tipo "e"
-const Reacao REACOES[10][7] = {
+const Reacao REACOES[10][8] = {
     {
         // Estado INICIAL
-        /*DIGITO*/ {NUM1, reacaoLerDigito},
-        /*OP    */ {NUM2REG2, reacaoLerOperacaoAtualizarOperando},
-        /*R     */ {REG1, NULL},
-        /*TIL   */ {ESPERA, NULL},
-        /*X     */ {ESPERA, NULL},
-        /*ENTER */ {INICIAL, NULL},
-        /*P     */ {INICIAL, NULL},
+        /*DIGITO */ {NUM1, reacaoLerDigito},
+        /*OP_ARIT*/ {NUM2REG2, reacaoLerOperacaoAtualizarOperando},
+        /*IGUAL  */ {NUM2REG2, reacaoLerOperacaoAtualizarOperando},
+        /*R      */ {REG1, NULL},
+        /*TIL    */ {ESPERA, NULL},
+        /*X      */ {ESPERA, NULL},
+        /*ENTER  */ {INICIAL, NULL},
+        /*P      */ {INICIAL, NULL},
     },
     {
         // Estado NUM1
-        /*DIGITO*/ {NUM1, reacaoLerDigito},
-        /*OP    */ {NUM2REG2, reacaoLerOperacaoSalvarNum},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {INICIAL, reacaoAtualizaAcumulador},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {NUM1, reacaoLerDigito},
+        /*OP_ARIT*/ {NUM2REG2, reacaoLerOperacaoSalvarNum},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {INICIAL, reacaoAtualizaAcumulador},
+        /*P      */ {TERMINAL, reacaoErro},
     },
     {
         // Estado NUM2
-        /*DIGITO*/ {NUM2, reacaoLerDigito},
-        /*OP    */ {TERMINAL, reacaoErro},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {INICIAL, reacaoRealizaOperacao},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {NUM2, reacaoLerDigito},
+        /*OP_ARIT*/ {TERMINAL, reacaoErro},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {INICIAL, reacaoRealizaOperacao},
+        /*P      */ {TERMINAL, reacaoErro},
     },
     {
         // Estado REG1
-        /*DIGITO*/ {REG1A, reacaoLerDigito},
-        /*OP    */ {TERMINAL, reacaoErro},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {TERMINAL, reacaoErro},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {REG1A, reacaoLerRegistrador},
+        /*OP_ARIT*/ {TERMINAL, reacaoErro},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {TERMINAL, reacaoErro},
+        /*P      */ {TERMINAL, reacaoErro},
     },
-        {
+    {
         // Estado REG1A
-        /*DIGITO*/ {TERMINAL, reacaoErro},
-        /*OP    */ {NUM2REG2, reacaoLerOperacao},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {TERMINAL, reacaoErro},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {TERMINAL, reacaoErro},
+        /*OP_ARIT*/ {NUM2REG2, reacaoLerOperacaoSalvarNum},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {INICIAL, reacaoAtualizaAcumulador},
+        /*P      */ {TERMINAL, reacaoErro},
     },
-        {
+    {
         // Estado REG2
-        /*DIGITO*/ {REG2A, reacaoLerDigito},
-        /*OP    */ {TERMINAL, reacaoErro},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {TERMINAL, reacaoErro},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {REG2A, reacaoLerRegistrador},
+        /*OP_ARIT*/ {TERMINAL, reacaoErro},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {TERMINAL, reacaoErro},
+        /*P      */ {TERMINAL, reacaoErro},
     },
-        {
+    {
         // Estado REG2A
-        /*DIGITO*/ {TERMINAL, reacaoErro},
-        /*OP    */ {TERMINAL, reacaoErro},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {INICIAL, NULL},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {TERMINAL, reacaoErro},
+        /*OP_ARIT*/ {TERMINAL, reacaoErro},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {INICIAL, reacaoRealizaOperacao},
+        /*P      */ {TERMINAL, reacaoErro},
     },
         {
         // Estado NUM2REG2
-        /*DIGITO*/ {NUM2, reacaoLerDigito},
-        /*OP    */ {TERMINAL, reacaoErro},
-        /*R     */ {REG2, NULL},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {TERMINAL, reacaoErro},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {NUM2, reacaoLerDigito},
+        /*OP_ARIT*/ {TERMINAL, reacaoErro},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {REG2, NULL},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {TERMINAL, reacaoErro},
+        /*P      */ {TERMINAL, reacaoErro},
     },
-        {
+    {
         // Estado ESPERA
-        /*DIGITO*/ {TERMINAL, reacaoErro},
-        /*OP    */ {TERMINAL, reacaoErro},
-        /*R     */ {TERMINAL, reacaoErro},
-        /*TIL   */ {TERMINAL, reacaoErro},
-        /*X     */ {TERMINAL, reacaoErro},
-        /*ENTER */ {INICIAL, NULL},
-        /*P     */ {TERMINAL, reacaoErro},
+        /*DIGITO */ {TERMINAL, reacaoErro},
+        /*OP_ARIT*/ {TERMINAL, reacaoErro},
+        /*IGUAL  */ {TERMINAL, NULL},
+        /*R      */ {TERMINAL, reacaoErro},
+        /*TIL    */ {TERMINAL, reacaoErro},
+        /*X      */ {TERMINAL, reacaoErro},
+        /*ENTER  */ {INICIAL, NULL},
+        /*P      */ {TERMINAL, reacaoErro},
     },
 };
 
@@ -191,7 +213,9 @@ int extrairEvento() {
         tipoDeEvento = DIGITO;
     } else if (charEvento == '+' || charEvento == '-' ||
                charEvento == '*' || charEvento == '/') {
-        tipoDeEvento = OP;
+        tipoDeEvento = OP_ARIT;
+    } else if (charEvento == '=') {
+        tipoDeEvento = IGUAL;
     } else if (charEvento == 'R') {
         tipoDeEvento = R;
     } else if (charEvento == '~') {
